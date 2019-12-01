95080597a89852793cb234b452f22960d7f6803d
3Lilio <alimohamed2019@gmail.com>
2019-12-01 11:22:24 +0200

    carDetails added

---
+++ Trip/Senarios/Profile/Controller/EditCarDetailsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  EditCarDetailsViewController.swift
+//  Trip
+//
+//  Created by Ali Mohamed on 12/1/19.
+//  Copyright © 2019 amirahmed. All rights reserved.
+//
+
+import UIKit
+
+class EditCarDetailsViewController: UIViewController {
+
+    @IBOutlet weak var ownerName: UITextField!
+    @IBOutlet weak var attachButn: UIButton!
+    @IBOutlet weak var model: UITextField!
+    @IBOutlet weak var color: UITextField!
+    @IBOutlet weak var rentPricePerDay: UITextField!
+    @IBOutlet weak var dateFrom: UITextField!
+    @IBOutlet weak var dateTo: UITextField!
+    @IBOutlet weak var phone: UITextField!
+    @IBOutlet weak var km: UITextField!
+    @IBOutlet weak var kmPrice: UITextField!
+    @IBOutlet weak var tripPrice: UITextField!
+    @IBOutlet weak var city: UITextField!
+    @IBOutlet weak var region: UITextField!
+    @IBOutlet weak var stName: UITextField!
+    @IBOutlet weak var apartmentNumber: UITextField!
+    @IBOutlet weak var detailsDescription: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+  
+    @IBAction func attachPressed(_ sender: UIButton) {
+    }
+    @IBAction func menuPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func savePressed(_ sender: UIButton) {
+    }
+}
---
+++ Trip/Senarios/Profile/Controller/ProfileViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ProfileViewController.swift
+//  Trip
+//
+//  Created by Ali Mohamed on 12/1/19.
+//  Copyright © 2019 amirahmed. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func editProfile(_ sender: UIButton) {
+    }
+    
+    @IBAction func editCarDetails(_ sender: UIButton) {
+    }
+    
+    @IBAction func reservations(_ sender: UIButton) {
+    }
+    
+    @IBAction func signOut(_ sender: UIButton) {
+    }
+    @IBAction func showMenuPressed(_ sender: UIBarButtonItem) {
+    }
+    
+}
