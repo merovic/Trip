363ce9d15d46e3a1213f0fa0bbd251b7de4b9015
Mostafa <mostafatarek15894@gmail.com>
2019-12-01 18:51:11 +0200

    add Search View

---
+++ Trip/Senarios/Search/Controller/SearchViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SearchViewController.swift
+//  Trip
+//
+//  Created by Mustafa on 12/1/19.
+//  Copyright © 2019 amirahmed. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    @IBOutlet weak var bigView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
