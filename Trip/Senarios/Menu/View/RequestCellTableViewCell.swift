c769cd2107fa5d9f97d3c1461a707b6a2a7df67a
Mostafa <mostafatarek15894@gmail.com>
2019-12-04 12:03:15 +0200

    add View Request

---
+++ Trip/Senarios/Menu/View/RequestCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RequestCellTableViewCell.swift
+//  Trip
+//
+//  Created by Mustafa on 12/4/19.
+//  Copyright © 2019 amirahmed. All rights reserved.
+//
+
+import UIKit
+
+class RequestCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
